5279ca81884f6286f954a1ce4ee4a3414011c3ea
BigRockSchoolGregR <gregorio.rossi@student.bigrock.it>
2024-05-14 14:27:52 +0200

    Upgrade RigidbodyTriggeredAttacker

--- Assets/Scripts/RigidbodyTriggeredAttacker.cs
+++ Assets/Scripts/RigidbodyTriggeredAttacker.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class RigidbodyTriggeredAttacker : MonoBehaviour
 {
+    [SerializeField] private GameObject AttackerRoot;
     [SerializeField] private int AttackPower = 1;
+    private void Start()
+    {
+        if (AttackerRoot == default(GameObject))
+        {
+            AttackerRoot = gameObject;
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent(out Damageable d))
         {
-            d.TakeDamage(AttackPower, gameObject);
+            d.TakeDamage(AttackPower, AttackerRoot);
         }
     }
     private void OnTriggerEnter(UnityEngine.Collider collision)
     {
         if (collision.gameObject.TryGetComponent(out Damageable d))
         {
-            d.TakeDamage(AttackPower, gameObject);
+            d.TakeDamage(AttackPower, AttackerRoot);
         }
     }
 }
