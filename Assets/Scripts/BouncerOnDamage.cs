6ebcdfc5b553da4ac0499c8774a02656851e503b
BigRockSchoolGregR <gregorio.rossi@student.bigrock.it>
2024-05-14 11:40:22 +0200

    Add BouncerOnDamage

---
+++ Assets/Scripts/BouncerOnDamage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+[RequireComponent(typeof(Damageable))]
+public class BouncerOnDamage : MonoBehaviour, IDamageListener
+{
+    [SerializeField] private float BouncePower = 20.0f;
+    public void OnDeath(GameObject killer)
+    {
+    }
+
+    public void OnHurt(int damageTaken, int health, int MaxHealth, GameObject attacker)
+    {
+        if (attacker.TryGetComponent(out Rigidbody rb))
+        {
+            rb.AddForce(-attacker.transform.forward * BouncePower, ForceMode.Impulse);
+        }
+    }
+}
